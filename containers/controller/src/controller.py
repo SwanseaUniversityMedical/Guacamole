4a7d3479ee3b7bd121251812364117bc34341618
Rowan Dash <rowan.dash@swansea.ac.uk>
2025-11-12 14:31:32 +0000

    test: different login method

--- containers/controller/src/controller.py
+++ containers/controller/src/controller.py
@@ -255,7 +255,7 @@ def main(
 
     logging.info("Load kube config")
     k8s.config.load_incluster_config()
-    kopf.login_with_kubeconfig()
+    kopf.login_via_client()
 
     @kopf.on.create('GuacamoleConnection')
     @kopf.on.update('GuacamoleConnection')
